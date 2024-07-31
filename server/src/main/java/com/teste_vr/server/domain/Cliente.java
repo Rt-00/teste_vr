400dd3a37f1521909f16aea520b9f941c0e7a4da
Rafael Toledo <toledorafael304@gmail.com>
2024-07-31 18:01:56 -0300

    feat: created Cliente entity

---
+++ server/src/main/java/com/teste_vr/server/domain/Cliente.java
@@ -0,0 +1,17 @@
+package com.teste_vr.server.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "cliente")
+public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false)
+    private double limiteCompra;
+}
